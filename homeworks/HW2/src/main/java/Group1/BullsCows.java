031c08ad882565e76251bee178051970daa150f1
olddirtyleva <Levzonin@mail.ru>
2020-10-12 09:19:44 +0300

    Created Project

---
+++ homeworks/HW2/src/main/java/Group1/BullsCows.java
@@ -0,0 +1,15 @@
+package Group1;
+
+import org.slf4j.LoggerFactory;
+
+public class BullsCows {
+    private static final org.slf4j.Logger log = LoggerFactory.getLogger(BullsCows.class);
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int forTest() {
+        return 9000;
+    }
+}
